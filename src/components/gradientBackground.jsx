c40df8a225051a7e9167ae97db9a41c900b28958
facu <facundodipi@gmail.com>
2022-07-06 21:31:39 -0300

    framer background

---
+++ src/components/gradientBackground.jsx
@@ -0,0 +1,17 @@
+import {animate,motion} from "framer-motion"
+import { Container,} from "@chakra-ui/react";
+
+const gradientBackground = ()=> {
+
+    return (
+<Container>
+    <motion.div>
+        
+    </motion.div>
+</Container>
+
+    );
+
+};
+
+export default gradientBackground
