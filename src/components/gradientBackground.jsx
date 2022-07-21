352e6bb13931d936c422e0ed2c6e9ce411490c8f
Facundo Di Pietrantonio <64317559+FacuDipi@users.noreply.github.com>
2022-07-21 10:27:20 -0300

    Update gradientBackground.jsx

--- src/components/gradientBackground.jsx
+++ src/components/gradientBackground.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import {motion, useAnimation} from "framer-motion"
-import { Container, Box} from "@chakra-ui/react";
-=======
-<<<<<<< HEAD
+
 
 import {motion, useAnimation} from "framer-motion"
 import { Container, Box} from "@chakra-ui/react";
@@ -37,43 +33,4 @@ return(
 )
 };
 export default GradientBackGround
-=======
-import {animate,motion} from "framer-motion"
-import { Container,} from "@chakra-ui/react";
->>>>>>> ff44dbaf5a852b3aadb2ddd614a3ed06cd5797ca
 
-
-
-
-const GradientBackGround = ()=>{
-    const control = useAnimation()
-
-return(
-
-    <Container classname= "back-container"
-    onChange={()=> control.start({
-
-
-    }
-       )}>
-
-    <motion.Box
-    classname= "BackBox"
-   
-    >
-
-<<<<<<< HEAD
-    
-
-
-    </motion.Box>
-    </Container>
-
-
-)
-};
-export default GradientBackGround
-=======
-export default gradientBackground
->>>>>>> c40df8a225051a7e9167ae97db9a41c900b28958
->>>>>>> ff44dbaf5a852b3aadb2ddd614a3ed06cd5797ca
