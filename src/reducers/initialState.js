fe17380d490982a5e5584371309093d42f271b88
facu <facundodipi@gmail.com>
2022-07-28 07:48:04 -0300

    config store

---
+++ src/reducers/initialState.js
@@ -0,0 +1,8 @@
+export const initialState {
+weather:{
+    tempNum:[],
+    cityName:[],
+    countryName:[]
+
+}
+};
--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,9 +1,15 @@
 
 import {configureStore} from '@reduxjs/toolkit';  
 import rootReducer from '../reducers';  
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
+let middlewares = [thunk];
+const middleware = applyMiddleware(...middlewares);
 
+export default createStore(rootReducer, composeWithDevTools(middleware));
 
-export default function getStore() {  
+export  function getStore() {  
   return configureStore(rootReducer);
 }
